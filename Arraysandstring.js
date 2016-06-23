c3294d8096159970377af92753c71db7380e55a7
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-06-23 16:07:04 -0700

    Matrix problems

---
+++ Arraysandstring.js
@@ -0,0 +1,54 @@
+//Rotate n X n matrix 90 degrees.
+var rotateMatrix = function (arr) {
+  var newArray = [];
+  var n = arr.length
+  for (var i=0; i < n; i++) {
+    newArray.push([]);
+  };
+  for (var i=0; i < n; i++) {
+    for ( var j= 0; j<n; j++ ) {
+      newArray[j][n-1-i] = arr[i][j];
+    };
+  };
+  return newArray;
+}
+var matrix = [[1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]];
+
+rotateMatrix(matrix);
+
+
+//When you see 0, make the rows and columns of it zero.
+var zeroMatrix = function (arr) {
+  var n = arr.length;
+  var m = arr[0].length
+  var rows = [];
+  var columns = [];
+
+  for (var i= 0; i < n; i++) {
+    for (var j= 0; j < m; j++) {
+      if (arr[i][j] === 0) {
+        rows.push(i);
+        columns.push(j);
+      };
+    };
+  }
+  for (var i= 0; i < rows.length; i++) {
+    for (var j = 0; j < m; j++) {
+      arr[rows[i]][j]=0;
+    };
+  }
+  for (var i= 0; i < columns.length; i++) {
+    for (var j = 0; j < n; j++) {
+      arr[j][columns[i]]=0;
+    };  
+  } 
+  return arr;
+}
+var matrix = [[1,0,3,4],
+        [5,6,7,8],
+        [9,10,11,12]];
+
+zeroMatrix(matrix);
