f18a05b6ea3da020725c3f89e0fb9ce9f2a533c9
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-07-01 16:55:10 -0700

    Recursion

---
+++ Recursion.js
@@ -0,0 +1,4 @@
+//Bottom-Up - How you can build solution for one case off of the previous case
+//Top-Down- How you can divide the problem for case N into sub problem
+//Half and Half (Binary Search, Merge Sort) - Divide data in 2 sets
+//Dynamic Programming - Find recursive algorithm, find over lapping sub problems, cache those results for future recursive calls.
