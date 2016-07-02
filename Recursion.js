c4b7f358f5a6c6dacdcffe94edb24453b6f58d41
Hazal Ozcan <hazal.ozcan@yahoo.com>
2016-07-01 18:18:20 -0700

    Recursive and Iterative Fib

--- Recursion.js
+++ Recursion.js
@@ -2,3 +2,33 @@
 //Top-Down- How you can divide the problem for case N into sub problem
 //Half and Half (Binary Search, Merge Sort) - Divide data in 2 sets
 //Dynamic Programming - Find recursive algorithm, find over lapping sub problems, cache those results for future recursive calls.
+
+var Fib = function (num) {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+  return Fib(n-1) + Fib(n-2);
+};
+
+
+//Recursive 
+var Fib = function(num) {
+  var prev1=1;
+  var prev2=1; 
+  if (num === 0) {
+      return 0;
+  }
+  if (num === 1 || num === 2) {
+    return 1;
+  } 
+  for (var i=3; i<=num; i++) {
+    var a = prev1;
+    prev1 += prev2;
+    prev2 = a;
+  }
+  return prev1;
+}
+
